5e49a11ff7d982690153fdd812f826993b9b0b71
Kalp <kalp10099@gmail.com>
2025-08-27 00:39:11 +0530

    Add files via upload

---
+++ 17-AreaPerimeter.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+    float L,P,A;
+    
+
+    printf("Enter length:");
+    scanf("%f",&L);
+    A=L*L;
+    P=4*L;
+    printf("area=%.2f",A);
+    printf("perimeter=%.2f",P);
+    
+    
+    
+    
+    
+}
---
+++ 19-AreaCircle.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+
+{
+    float P,A,R;
+    
+
+    printf("Enter Radius:");
+    scanf("%f",&R);
+    
+    
+    A=22*R*R/7;
+    P=44*R/7;
+    printf("area=%.2f",A);
+    printf("perimeter=%.2f",P);
+    
+    
+    
+    
+    
+}
+
